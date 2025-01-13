0e0c820b932da31cb61dfa6e8b71c24c5ef70586
Kacper Mazur <kacper.mazur888@gmail.com>
2025-01-13 11:41:39 +0000

    layout metadata updated

--- codelka/app/layout.tsx
+++ codelka/app/layout.tsx
@@ -4,6 +4,11 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Codelka',
   description: 'Visit Codelka.com and view all information about us and our games.',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  }
 }
 
 export default function RootLayout({
